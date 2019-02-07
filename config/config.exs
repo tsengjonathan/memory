2c3a9dbc9f2bf5f20f5957c7f5e2b470aa005380
tsengjonathan <tseng.j@husky.neu.edu>
2019-02-07 15:14:57 -0500

    Adjusted secret token generation

--- config/config.exs
+++ config/config.exs
@@ -5,10 +5,17 @@
 # is restricted to this project.
 use Mix.Config
 
+path = Path.expand("~/.config/memory.secret")
+unless File.exists?(path) do
+  secret = Base.encode16(:crypto.strong_rand_bytes(32))
+  File.write!(path, secret)
+end
+secret = File.read!(path)
+
 # Configures the endpoint
 config :memory, MemoryWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: get_secret(),
+  secret_key_base: secret,
   render_errors: [view: MemoryWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Memory.PubSub,
            adapter: Phoenix.PubSub.PG2]
@@ -24,16 +31,3 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
-
-get_secret = fn name ->
-  # Secret generation hack by Nat Tuck for CS4550
-  # This function is dedicated to the public domain.
-  base = Path.expand("~/.config/phx-secrets")
-  File.mkdir_p!(base)
-  path = Path.join(base, name)
-  unless File.exists?(path) do
-    secret = Base.encode16(:crypto.strong_rand_bytes(32))
-    File.write!(path, secret)
-  end
-  String.trim(File.read!(path))
-end
